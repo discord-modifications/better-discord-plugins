7493fbf8d42ca94ee47c5a41faf394714cdbf70d
eternal <73202350+slow@users.noreply.github.com>
2021-02-17 22:17:39 +0000

    Create GameActivityToggle.plugin.js

---
+++ GameActivityToggle.plugin.js
@@ -0,0 +1,128 @@
+/**
+ * @name GameActivityToggle
+ * @source https://github.com/slow/better-discord-plugins/blob/main/GameActivityToggle.plugin.js
+ * @updateUrl https://raw.githubusercontent.com/slow/better-discord-plugins/main/GameActivityToggle.plugin.js
+ * @website https://github.com/slow/better-discord-plugins/tree/main/GameActivityToggle.plugin.js
+ * @authorId 282595588950982656
+ * @donate https://paypal.me/eternal404
+ */
+
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+
+const config = {
+   info: {
+      name: 'GameActivityToggle',
+      authors: [
+         {
+            name: 'eternal',
+            discord_id: '282595588950982656',
+         }
+      ],
+      version: '1.0.0',
+      description: 'Adds an entry in the status picker to toggle game activity.',
+      github: 'https://github.com/slow/better-discord-plugins/tree/main/GameActivityToggle.plugin.js',
+      github_raw: 'https://raw.githubusercontent.com/slow/better-discord-plugins/main/GameActivityToggle.plugin.js',
+   }
+};
+
+module.exports = !global.ZeresPluginLibrary ? class {
+   getName() {
+      return config.info.name;
+   }
+
+   getAuthor() {
+      return config.info.authors[0].name;
+   }
+
+   getVersion() {
+      return config.info.version;
+   }
+
+   getDescription() {
+      return config.info.description;
+   }
+
+   load() {
+      BdApi.showConfirmationModal('Library plugin is needed',
+         `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
+         confirmText: 'Download',
+         cancelText: 'Cancel',
+         onConfirm: () => {
+            request.get('https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js', (error, response, body) => {
+               if (error) {
+                  return electron.shell.openExternal('https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js');
+               }
+               fs.writeFileSync(path.join(BdApi.Plugins.folder, '0PluginLibrary.plugin.js'), body);
+            });
+         }
+      });
+   }
+
+   start() { }
+
+   stop() { }
+} : (([Plugin, API]) => {
+   const { Patcher, WebpackModules, DiscordModules: { React } } = API;
+
+   return class GameActivityToggle extends Plugin {
+      constructor() {
+         super();
+      }
+
+      start() {
+         let showCurrentGame;
+         const g = WebpackModules.getByProps('showCurrentGame');
+         const settings = WebpackModules.getByProps('updateRemoteSettings');
+         const classes = WebpackModules.getByProps('status', 'description');
+         const Menu = WebpackModules.getByProps('MenuItem');
+         const enabledIcon = w => React.createElement('svg', {
+            viewBox: '0 0 24 24', width: w, height: w, style: { 'margin-left': '-2px' }
+         }, React.createElement('path', {
+            style: { fill: 'currentColor' },
+            d: 'M20.8,7.7c-0.6-1.2-1.8-1.9-3.1-1.9H6.3C5,5.7,3.8,6.5,3.2,7.6l-2.8,5.8c0,0,0,0,0,0C-0.3,15.1,0.4,17,2,17.8L2.3,18C4,18.7,5.9,18,6.7,16.4l0.1-0.3c0.3-0.6,0.9-1,1.6-1h7.1c0.7,0,1.3,0.4,1.6,1l0.1,0.3c0.8,1.6,2.7,2.4,4.4,1.6l0.3-0.1c1.6-0.8,2.3-2.7,1.6-4.4L20.8,7.7z M8.6,10.5c0,0.2-0.2,0.4-0.4,0.4H7.3c-0.2,0-0.4,0.2-0.4,0.4v0.9c0,0.2-0.2,0.4-0.4,0.4H5.7c-0.2,0-0.4-0.2-0.4-0.4v-0.9c0-0.2-0.2-0.4-0.4-0.4c0,0,0,0,0,0H4.1c-0.2,0-0.4-0.2-0.4-0.4V9.7c0-0.2,0.2-0.4,0.4-0.4h0.9c0.2,0,0.4-0.2,0.4-0.4c0,0,0,0,0,0V8.1c0-0.2,0.2-0.4,0.4-0.4h0.8C6.8,7.7,7,7.9,7,8.1V9c0,0.2,0.2,0.4,0.4,0.4h0.9c0.2,0,0.3,0.2,0.3,0.4V10.5z M15.6,10.9c-0.4,0-0.8-0.3-0.8-0.8c0-0.4,0.3-0.8,0.8-0.8c0,0,0,0,0,0c0.4,0,0.8,0.3,0.8,0.8C16.4,10.5,16.1,10.9,15.6,10.9z M17.2,7.7C17.2,7.7,17.2,7.7,17.2,7.7c0.4,0,0.8,0.3,0.8,0.8c0,0,0,0,0,0c0,0.4-0.4,0.8-0.8,0.8c-0.4,0-0.8-0.4-0.8-0.8S16.8,7.7,17.2,7.7z M18,11.7L18,11.7C18,11.7,18,11.7,18,11.7c0,0.4-0.3,0.8-0.8,0.8c-0.4,0-0.8-0.3-0.8-0.8c0-0.4,0.3-0.8,0.8-0.8c0,0,0,0,0,0C17.7,10.9,18,11.3,18,11.7C18,11.7,18,11.7,18,11.7L18,11.7C18,11.7,18,11.7,18,11.7C18,11.7,18,11.7,18,11.7z M18.9,10.9c-0.4,0-0.8-0.3-0.8-0.8c0-0.4,0.3-0.8,0.8-0.8c0,0,0,0,0,0c0.4,0,0.8,0.3,0.8,0.8C19.6,10.5,19.3,10.9,18.9,10.9z'
+         }));
+         const disabledIcon = w => React.createElement('svg', {
+            viewBox: '0 0 24 24', width: w, height: w, style: { 'margin-left': '-2px' }
+         }, React.createElement('path', {
+            style: { fill: 'currentColor' },
+            d: 'M17.7,5.7h-0.8L4.4,18.1c1-0.2,1.9-0.8,2.3-1.8l0.1-0.3c0.3-0.6,0.9-1,1.6-1h1.9l4.7-4.6v0c-0.1-0.1-0.1-0.2-0.1-0.4c0-0.4,0.3-0.8,0.8-0.8c0,0,0,0,0,0c0.1,0,0.2,0,0.3,0.1l0.5-0.5c-0.1-0.1-0.1-0.2-0.1-0.4c0-0.4,0.3-0.8,0.8-0.8c0.1,0,0.3,0,0.4,0.1l1.7-1.7C18.8,5.8,18.3,5.7,17.7,5.7z M23.5,13.4l-2.8-5.8c0,0,0-0.1-0.1-0.1l-1.8,1.8c0.4,0,0.7,0.4,0.7,0.8c0,0.4-0.3,0.8-0.8,0.8c-0.4,0-0.8-0.3-0.8-0.7l-0.8,0.8c0.4,0,0.7,0.4,0.7,0.8c0,0.4-0.4,0.8-0.8,0.8c-0.4,0-0.8-0.3-0.8-0.7L13.1,15h2.4c0.7,0,1.3,0.4,1.6,1l0.1,0.3c0.8,1.6,2.7,2.3,4.4,1.6l0.3-0.1C23.6,17,24.3,15,23.5,13.4z M6.3,5.7C5,5.7,3.8,6.4,3.3,7.6l-2.8,5.8c0,0,0,0,0,0C-0.3,15,0.4,16.9,2,17.7L14,5.7H6.3z M8.2,10.8H7.3c-0.2,0-0.4,0.2-0.4,0.3v0.9c0,0.2-0.2,0.3-0.3,0.3H5.7c-0.2,0-0.3-0.2-0.3-0.3v-0.9c0-0.2-0.2-0.3-0.4-0.3H4.1c-0.2,0-0.4-0.2-0.4-0.4V9.6c0-0.2,0.2-0.4,0.4-0.4H5c0.2,0,0.4-0.2,0.4-0.4V8c0-0.2,0.2-0.4,0.4-0.4h0.8C6.8,7.7,7,7.8,7,8v0.9c0,0.2,0.2,0.4,0.4,0.4h0.9c0.2,0,0.3,0.2,0.3,0.4v0.8C8.6,10.7,8.4,10.8,8.2,10.8z'
+         }), React.createElement('polygon', {
+            style: { fill: '#F04747' },
+            points: '22.6,2.7 22.6,2.8 19.3,6.1 16,9.3 16,9.4 15,10.4 15,10.4 10.3,15 2.8,22.5 1.4,21.1 21.2,1.3 '
+         }));
+         Patcher.before(Menu, 'default', (_, args) => {
+            if (args[0].navId != 'status-picker') return args;
+
+            const [{ children }] = args;
+            const invisibleStatus = children.find(c => c.props.id == 'invisible');
+
+            if (!children.find(c => c.props.id == 'game-activity')) {
+               showCurrentGame = g.showCurrentGame;
+
+               children.splice(children.indexOf(invisibleStatus) + 1, 0, React.createElement(Menu.MenuItem, {
+                  id: 'game-activity',
+                  keepItemStyles: true,
+                  action: () => {
+                     showCurrentGame = !showCurrentGame;
+                     return settings.updateRemoteSettings({ showCurrentGame });
+                  },
+                  render: () => React.createElement('div', {
+                     className: classes.statusItem,
+                     'aria-label': `${showCurrentGame ? 'Hide' : 'Show'} Game Activity`
+                  }, showCurrentGame ? disabledIcon('16') : enabledIcon('16'), React.createElement('div', {
+                     className: classes.status
+                  }, `${showCurrentGame ? 'Hide' : 'Show'} Game Activity`), React.createElement('div', {
+                     className: classes.description
+                  }, 'Display currently running game as a status message.'))
+               }));
+            }
+         });
+      };
+
+      stop() {
+         Patcher.unpatchAll();
+      };
+   };
+})(global.ZeresPluginLibrary.buildPlugin(config));
