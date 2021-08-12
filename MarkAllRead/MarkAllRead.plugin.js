d0bc4f1d21a2061950e09a36dc392a37e9af0352
eternal <73202350+slow@users.noreply.github.com>
2021-08-12 18:29:06 +0100

    revert rewrites since they only caused more issues

--- MarkAllRead/MarkAllRead.plugin.js
+++ MarkAllRead/MarkAllRead.plugin.js
@@ -1,152 +1,181 @@
 /**
  * @name MarkAllRead
- * @description Silences your typing indicator/status.
- * @version 2.0.0
  * @source https://github.com/slow/better-discord-plugins/blob/master/MarkAllRead/MarkAllRead.plugin.js
  * @updateUrl https://raw.githubusercontent.com/slow/better-discord-plugins/master/MarkAllRead/MarkAllRead.plugin.js
- * @author eternal
+ * @website https://github.com/slow/better-discord-plugins/tree/master/MarkAllRead/MarkAllRead.plugin.js
  * @authorId 282595588950982656
  * @donate https://paypal.me/eternal404
  */
 
 /*@cc_on
 @if (@_jscript)
-
-    // Offer to self-install for clueless users that try to run this directly.
-    var shell = WScript.CreateObject("WScript.Shell");
-    var fs = new ActiveXObject("Scripting.FileSystemObject");
-    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-    var pathSelf = WScript.ScriptFullName;
-    // Put the user at ease by addressing them in the first person
-    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-    } else if (!fs.FolderExists(pathPlugins)) {
-        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-        // Show the user where to put plugins in the future
-        shell.Exec("explorer " + pathPlugins);
-        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-    }
-    WScript.Quit();
+	
+  // Offer to self-install for clueless users that try to run this directly.
+  var shell = WScript.CreateObject("WScript.Shell");
+  var fs = new ActiveXObject("Scripting.FileSystemObject");
+  var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+  var pathSelf = WScript.ScriptFullName;
+  // Put the user at ease by addressing them in the first person
+  shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+  if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+     shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+  } else if (!fs.FolderExists(pathPlugins)) {
+     shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+  } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+     fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+     // Show the user where to put plugins in the future
+     shell.Exec("explorer " + pathPlugins);
+     shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+  }
+  WScript.Quit();
 
 @else@*/
 
-const { findModuleByProps } = BdApi;
-const { getMutableGuildAndPrivateChannels } = findModuleByProps('getChannel');
-const unreadAcks = findModuleByProps('ack', 'ackCategory');
-const messageStore = findModuleByProps('hasUnread', 'lastMessageId');
+module.exports = (() => {
+   const config = {
+      main: 'index.js',
+      info: {
+         name: 'MarkAllRead',
+         authors: [
+            {
+               name: 'eternal',
+               discord_id: '282595588950982656',
+               github_username: 'slow',
+               twitter_username: ''
+            }
+         ],
+         version: '3.0.0',
+         description: 'Adds the command "read" that reads channels, DMs and removes pings.',
+         github: 'https://github.com/slow',
+         github_raw: 'https://raw.githubusercontent.com/slow/better-discord-plugins/master/MarkAllRead/MarkAllRead.plugin.js'
+      }
+   };
 
-class MarkAllRead {
-   commands = new (class Commands {
-      constructor() {
-         this.messages = BdApi.findModuleByProps('sendMessage');
-         this.messageUtils = BdApi.findModuleByProps('createBotMessage');
-         this.avatars = BdApi.findModuleByProps('BOT_AVATARS');
-
-         const DiscordCommands = BdApi.findModuleByProps('BUILT_IN_COMMANDS');
-         if (!DiscordCommands.BUILT_IN_SECTIONS['betterdiscord']) {
-            DiscordCommands.BUILT_IN_SECTIONS['betterdiscord'] = ({
-               icon: 'https://github.com/BetterDiscord.png',
-               id: 'betterdiscord',
-               name: 'BetterDiscord',
-               type: 0
-            });
+   const buildPlugin = ([Plugin, API]) => {
+      return class MarkAllRead extends Plugin {
+         constructor() {
+            super();
          }
 
-         this.avatars.BOT_AVATARS.better_discord = 'https://github.com/BetterDiscord.png';
-      }
+         async start() {
+            const { findModuleByProps } = BdApi;
+            const { getMutableGuildAndPrivateChannels } = findModuleByProps('getChannel');
+            const unreadAcks = findModuleByProps('ack', 'ackCategory');
+            const messageStore = findModuleByProps('hasUnread', 'lastMessageId');
 
-      register(caller, options) {
-         const DiscordCommands = BdApi.findModuleByProps('BUILT_IN_COMMANDS');
-
-         let old = options.execute;
-         if (!old) return;
-
-         options.execute = async (...args) => {
-            let res = null;
-
-            try {
-               res = await old(...args);
-            } catch (e) {
-               res = {
-                  send: false,
-                  result: `An error occurred while executing the command: ${e.message}.\nCheck the console for more details.`
-               };
-            }
-
-            let channelId = args[1].channel.id;
-            if (res && typeof res == 'object' && !Array.isArray(res) && res.send && typeof res.result == 'string') {
-               this.messages.sendMessage(channelId, {
-                  content: res.result,
-                  tts: false,
-                  validNonShortcutEmojis: [],
-                  invalidEmojis: []
-               });
-            } else if (res?.result || typeof res == 'string') {
-               let message = this.messageUtils.createBotMessage(channelId, '');
-               message.author.username = 'BetterDiscord';
-               message.author.avatar = 'better_discord';
-
-               if (typeof res.result === 'string') {
-                  message.content = res.result;
-               } else {
-                  message.embeds.push(res.result);
+            commands.register({
+               command: 'read',
+               description: 'Read all channels & remove pings',
+               usage: '{c}',
+               executor: async () => {
+                  let channels = Object.keys(getMutableGuildAndPrivateChannels());
+                  const unreads = channels.map(c => ({
+                     channelId: c,
+                     messageId: messageStore.lastMessageId(c)
+                  }));
+                  return await unreadAcks.bulkAck(unreads);
                }
+            });
+         };
 
-               this.messages.receiveMessage(channelId, message);
+         stop() {
+            commands.unregister('read');
+         };
+      };
+   };
+
+   return !global.ZeresPluginLibrary || !global.XenoLib || !global.commands ? class {
+      constructor() {
+         this._XL_PLUGIN = true;
+         this.start = this.load = this.handleMissingLib;
+      }
+
+      getName() {
+         return this.name.replace(/\s+/g, '');
+      }
+
+      getAuthor() {
+         return this.author;
+      }
+
+      getVersion() {
+         return this.version;
+      }
+
+      getDescription() {
+         return this.description + ' You are missing libraries for this plugin, please enable the plugin and click Download Now.';
+      }
+
+      start() { }
+
+      stop() { }
+
+      handleMissingLib() {
+         let missing = {
+            ZeresPluginLibrary: false,
+            CommandsAPI: false
+         };
+         if (!global.ZeresPluginLibrary) missing.ZeresPluginLibrary = true;
+         if (!global.commands) missing.CommandsAPI = true;
+         let missingCount = 0;
+         Object.values(missing).map(m => m ? missingCount++ : '');
+
+         BdApi.showConfirmationModal(missingCount == 1 ? 'Library plugin needed' : 'Library plugins needed',
+            `The library plugin${missingCount > 1 ? 's' : ''} needed for ${config.info.name} is missing. Please click Download to install the dependecies.`, {
+            confirmText: 'Download',
+            cancelText: 'Cancel',
+            onConfirm: async () => {
+               if (missing.ZeresPluginLibrary) {
+                  await new Promise((fulfill, reject) => {
+                     require('request').get('https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js', async (error, response, body) => {
+                        if (error) {
+                           return electron.shell.openExternal('https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js');
+                        }
+                        require('fs').writeFile(require('path').join(BdApi.Plugins.folder, '0PluginLibrary.plugin.js'), body, fulfill);
+                     });
+                  });
+               }
+               if (missing.CommandsAPI) {
+                  await new Promise((fulfill, reject) => {
+                     require('request').get('https://raw.githubusercontent.com/slow/better-discord-plugins/master/CommandsAPI/CommandsAPI.plugin.js', async (error, response, body) => {
+                        if (error) {
+                           return electron.shell.openExternal('https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/slow/better-discord-plugins/master/CommandsAPI/CommandsAPI.plugin.js');
+                        }
+                        require('fs').writeFile(require('path').join(BdApi.Plugins.folder, '2CommandsAPI.plugin.js'), body, fulfill);
+                     });
+                  });
+               }
             }
-         };
-
-         const { randomBytes } = require('crypto');
-         const cmd = Object.assign({
-            description: 'No description provided.',
-            id: randomBytes(16).toString('hex')
-         }, options, {
-            __registerId: caller,
-            applicationId: 'betterdiscord',
-            type: 3,
-            target: 1
          });
-
-         DiscordCommands.BUILT_IN_COMMANDS.push(cmd);
-
-         return () => {
-            const index = DiscordCommands.BUILT_IN_COMMANDS.indexOf(cmd);
-            if (index < 0) return false;
-            DiscordCommands.BUILT_IN_COMMANDS.splice(index, 1);
-         };
       }
 
-      unregister(caller) {
-         const DiscordCommands = BdApi.findModuleByProps('BUILT_IN_COMMANDS');
-
-         let index = DiscordCommands.BUILT_IN_COMMANDS.findIndex(cmd => cmd.__registerId === caller);
-
-         while (index > -1) {
-            DiscordCommands.BUILT_IN_COMMANDS.splice(index, 1);
-            index = DiscordCommands.BUILT_IN_COMMANDS.findIndex(cmd => cmd.__registerId === caller);
-         }
+      get [Symbol.toStringTag]() {
+         return 'Plugin';
       }
-   })();
 
-   start() {
-      this.commands.register('read', {
-         name: 'read',
-         description: 'Read all channels & remove pings.',
-         execute: async () => {
-            let channels = Object.keys(getMutableGuildAndPrivateChannels());
-            const unreads = channels.map(c => ({
-               channelId: c,
-               messageId: messageStore.lastMessageId(c)
-            }));
-            await unreadAcks.bulkAck(unreads);
+      get name() {
+         return config.info.name;
+      }
+
+      get short() {
+         let string = '';
+         for (let i = 0, len = config.info.name.length; i < len; i++) {
+            const char = config.info.name[i];
+            if (char === char.toUpperCase()) string += char;
          }
-      });
-   }
+         return string;
+      }
 
-   stop() {
-      this.commands.unregister('read');
-   }
-};
+      get author() {
+         return config.info.authors.map(author => author.name).join(', ');
+      }
+
+      get version() {
+         return config.info.version;
+      }
+
+      get description() {
+         return config.info.description;
+      }
+   } : buildPlugin(global.ZeresPluginLibrary.buildPlugin(config));
+})();
--- Mock/Mock.plugin.js
+++ Mock/Mock.plugin.js
@@ -1,157 +1,172 @@
 /**
  * @name Mock
- * @description Silences your typing indicator/status.
- * @version 2.0.0
  * @source https://github.com/slow/better-discord-plugins/blob/master/Mock/Mock.plugin.js
  * @updateUrl https://raw.githubusercontent.com/slow/better-discord-plugins/master/Mock/Mock.plugin.js
- * @author eternal
+ * @website https://github.com/slow/better-discord-plugins/tree/master/Mock/Mock.plugin.js
  * @authorId 282595588950982656
  * @donate https://paypal.me/eternal404
  */
 
 /*@cc_on
 @if (@_jscript)
-
-    // Offer to self-install for clueless users that try to run this directly.
-    var shell = WScript.CreateObject("WScript.Shell");
-    var fs = new ActiveXObject("Scripting.FileSystemObject");
-    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-    var pathSelf = WScript.ScriptFullName;
-    // Put the user at ease by addressing them in the first person
-    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-    } else if (!fs.FolderExists(pathPlugins)) {
-        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-        // Show the user where to put plugins in the future
-        shell.Exec("explorer " + pathPlugins);
-        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-    }
-    WScript.Quit();
+	
+  // Offer to self-install for clueless users that try to run this directly.
+  var shell = WScript.CreateObject("WScript.Shell");
+  var fs = new ActiveXObject("Scripting.FileSystemObject");
+  var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\BetterDiscord\plugins");
+  var pathSelf = WScript.ScriptFullName;
+  // Put the user at ease by addressing them in the first person
+  shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+  if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+     shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+  } else if (!fs.FolderExists(pathPlugins)) {
+     shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+  } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+     fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+     // Show the user where to put plugins in the future
+     shell.Exec("explorer " + pathPlugins);
+     shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+  }
+  WScript.Quit();
 
 @else@*/
 
-const { findModuleByProps } = BdApi;
-const { ApplicationCommandOptionType: OptionTypes } = findModuleByProps('ApplicationCommandType');
-
-class Mock {
-   commands = new (class Commands {
-      constructor() {
-         this.messages = BdApi.findModuleByProps('sendMessage');
-         this.messageUtils = BdApi.findModuleByProps('createBotMessage');
-         this.avatars = BdApi.findModuleByProps('BOT_AVATARS');
-
-         const DiscordCommands = BdApi.findModuleByProps('BUILT_IN_COMMANDS');
-         if (!DiscordCommands.BUILT_IN_SECTIONS['betterdiscord']) {
-            DiscordCommands.BUILT_IN_SECTIONS['betterdiscord'] = ({
-               icon: 'https://github.com/BetterDiscord.png',
-               id: 'betterdiscord',
-               name: 'BetterDiscord',
-               type: 0
-            });
-         }
-
-         this.avatars.BOT_AVATARS.better_discord = 'https://github.com/BetterDiscord.png';
-      }
-
-      register(caller, options) {
-         const DiscordCommands = BdApi.findModuleByProps('BUILT_IN_COMMANDS');
-
-         let old = options.execute;
-         if (!old) return;
-
-         options.execute = async (...args) => {
-            let res = null;
-
-            try {
-               res = await old(...args);
-            } catch (e) {
-               res = {
-                  send: false,
-                  result: `An error occurred while executing the command: ${e.message}.\nCheck the console for more details.`
-               };
-            }
-
-            let channelId = args[1].channel.id;
-            if (res && typeof res == 'object' && !Array.isArray(res) && res.send && typeof res.result == 'string') {
-               this.messages.sendMessage(channelId, {
-                  content: res.result,
-                  tts: false,
-                  validNonShortcutEmojis: [],
-                  invalidEmojis: []
-               });
-            } else if (res?.result || typeof res == 'string') {
-               let message = this.messageUtils.createBotMessage(channelId, '');
-               message.author.username = 'BetterDiscord';
-               message.author.avatar = 'better_discord';
-
-               if (typeof res.result === 'string') {
-                  message.content = res.result;
-               } else {
-                  message.embeds.push(res.result);
-               }
-
-               this.messages.receiveMessage(channelId, message);
-            }
-         };
-
-         const { randomBytes } = require('crypto');
-         const cmd = Object.assign({
-            description: 'No description provided.',
-            id: randomBytes(16).toString('hex')
-         }, options, {
-            __registerId: caller,
-            applicationId: 'betterdiscord',
-            type: 3,
-            target: 1
-         });
-
-         DiscordCommands.BUILT_IN_COMMANDS.push(cmd);
-
-         return () => {
-            const index = DiscordCommands.BUILT_IN_COMMANDS.indexOf(cmd);
-            if (index < 0) return false;
-            DiscordCommands.BUILT_IN_COMMANDS.splice(index, 1);
-         };
-      }
-
-      unregister(caller) {
-         const DiscordCommands = BdApi.findModuleByProps('BUILT_IN_COMMANDS');
-
-         let index = DiscordCommands.BUILT_IN_COMMANDS.findIndex(cmd => cmd.__registerId === caller);
-
-         while (index > -1) {
-            DiscordCommands.BUILT_IN_COMMANDS.splice(index, 1);
-            index = DiscordCommands.BUILT_IN_COMMANDS.findIndex(cmd => cmd.__registerId === caller);
-         }
-      }
-   })();
-
-   start() {
-      this.commands.register('mock', {
-         name: 'mock',
-         description: 'Mock a user...',
-         execute: (args) => {
-            let text = args.text[0].text;
-            return {
-               send: true,
-               result: text.split('').map((c, i) => i % 2 ? c.toUpperCase() : c.toLowerCase()).join('')
-            };
-         },
-         options: [
+module.exports = (() => {
+   const config = {
+      main: 'index.js',
+      info: {
+         name: 'Mock',
+         authors: [
             {
-               type: OptionTypes.STRING,
-               name: 'text',
-               description: 'The text to mock.',
-               required: true
+               name: 'eternal',
+               discord_id: '282595588950982656',
+               github_username: 'slow',
+               twitter_username: ''
             }
-         ]
-      });
-   }
+         ],
+         version: '3.0.0',
+         description: 'Adds the command "mock" that uppercases & lowercases letters to mock someone.',
+         github: 'https://github.com/slow',
+         github_raw: 'https://raw.githubusercontent.com/slow/better-discord-plugins/master/Mock/Mock.plugin.js'
+      }
+   };
 
-   stop() {
-      this.commands.unregister('mock');
-   }
-};
+   const buildPlugin = ([Plugin, API]) => {
+      return class Mock extends Plugin {
+         constructor() {
+            super();
+         }
+
+         async start() {
+            commands.register({
+               command: 'mock',
+               description: 'Mock a user...',
+               usage: '{c} [text to mock]',
+               executor: (args) => ({
+                  send: true,
+                  result: args.join(' ').split('').map((c, i) => i % 2 ? c.toUpperCase() : c.toLowerCase()).join('')
+               })
+            });
+         };
+
+         stop() {
+            commands.unregister('mock');
+         };
+      };
+   };
+
+   return !global.ZeresPluginLibrary || !global.XenoLib || !global.commands ? class {
+      constructor() {
+         this._XL_PLUGIN = true;
+         this.start = this.load = this.handleMissingLib;
+      }
+
+      getName() {
+         return this.name.replace(/\s+/g, '');
+      }
+
+      getAuthor() {
+         return this.author;
+      }
+
+      getVersion() {
+         return this.version;
+      }
+
+      getDescription() {
+         return this.description + ' You are missing libraries for this plugin, please enable the plugin and click Download Now.';
+      }
+
+      start() { }
+
+      stop() { }
+
+      handleMissingLib() {
+         let missing = {
+            ZeresPluginLibrary: false,
+            CommandsAPI: false
+         };
+         if (!global.ZeresPluginLibrary) missing.ZeresPluginLibrary = true;
+         if (!global.commands) missing.CommandsAPI = true;
+         let missingCount = 0;
+         Object.values(missing).map(m => m ? missingCount++ : '');
+
+         BdApi.showConfirmationModal(missingCount == 1 ? 'Library plugin needed' : 'Library plugins needed',
+            `The library plugin${missingCount > 1 ? 's' : ''} needed for ${config.info.name} is missing. Please click Download to install the dependecies.`, {
+            confirmText: 'Download',
+            cancelText: 'Cancel',
+            onConfirm: async () => {
+               if (missing.ZeresPluginLibrary) {
+                  await new Promise((fulfill, reject) => {
+                     require('request').get('https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js', async (error, response, body) => {
+                        if (error) {
+                           return electron.shell.openExternal('https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js');
+                        }
+                        require('fs').writeFile(require('path').join(BdApi.Plugins.folder, '0PluginLibrary.plugin.js'), body, fulfill);
+                     });
+                  });
+               }
+               if (missing.CommandsAPI) {
+                  await new Promise((fulfill, reject) => {
+                     require('request').get('https://raw.githubusercontent.com/slow/better-discord-plugins/master/CommandsAPI/CommandsAPI.plugin.js', async (error, response, body) => {
+                        if (error) {
+                           return electron.shell.openExternal('https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/slow/better-discord-plugins/master/CommandsAPI/CommandsAPI.plugin.js');
+                        }
+                        require('fs').writeFile(require('path').join(BdApi.Plugins.folder, '2CommandsAPI.plugin.js'), body, fulfill);
+                     });
+                  });
+               }
+            }
+         });
+      }
+
+      get [Symbol.toStringTag]() {
+         return 'Plugin';
+      }
+
+      get name() {
+         return config.info.name;
+      }
+
+      get short() {
+         let string = '';
+         for (let i = 0, len = config.info.name.length; i < len; i++) {
+            const char = config.info.name[i];
+            if (char === char.toUpperCase()) string += char;
+         }
+         return string;
+      }
+
+      get author() {
+         return config.info.authors.map(author => author.name).join(', ');
+      }
+
+      get version() {
+         return config.info.version;
+      }
+
+      get description() {
+         return config.info.description;
+      }
+   } : buildPlugin(global.ZeresPluginLibrary.buildPlugin(config));
+})();
